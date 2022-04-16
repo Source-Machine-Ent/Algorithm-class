acfdc340f04ca026ce2c8d8e9517c97348f98b3c
210_reverof <sheltonwon@gmail.com>
2022-04-16 17:38:59 +0900

    BOJ 1735

---
+++ wonyoung/numbers/1735.java
@@ -0,0 +1,48 @@
+// 220416 분수 합
+
+package wonyoung.number;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static int aTop, aBottom, bTop, bBottom, resTop, resBottom;
+
+    public static void main(String[] args) throws Exception {
+        // for coding
+        System.setIn(new FileInputStream("./input/input_1735.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ", false);
+
+        aTop = Integer.parseInt(st.nextToken());
+        aBottom = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ", false);
+
+        bTop = Integer.parseInt(st.nextToken());
+        bBottom = Integer.parseInt(st.nextToken());
+
+        resTop = aTop * bBottom + bTop * aBottom;
+        resBottom = aBottom * bBottom;
+
+        int mod = gcd(resTop, resBottom);
+        resTop /= mod;
+        resBottom /= mod;
+
+        bw.write(resTop + " " + resBottom);
+
+        bw.close();
+    }
+    
+    public static int gcd(int a, int b) {
+        if (a <= b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        return b==0? a : gcd(b , a % b);
+    }
+
+}
