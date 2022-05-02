1e63b87e936241be8328fe6f92bc4e0b53cfe09a
ningpop <craft1933@naver.com>
2022-05-02 22:39:00 +0900

    [#82] 3955 캔디 분배 - 미해결

---
+++ ningpop/3955.py
@@ -0,0 +1,13 @@
+# 2022.05.02
+# 풀이 시간 분 초
+# 채점 결과: 
+# 시간복잡도: 
+# 문제 링크: https://www.acmicpc.net/problem/3955
+
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    k, c = map(int, input().split())
