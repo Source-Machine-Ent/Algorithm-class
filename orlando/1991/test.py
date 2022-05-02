347250687b995aab5a56dc2d0020d5dcdc86d1a6
ingkoon <ing97220@naver.com>
2022-05-02 10:29:09 +0900

    [2022-05-02] boj 1991

--- orlando/1991/test.py
+++ orlando/1991/test.py
@@ -23,12 +23,38 @@
 '''
 
 import sys
-from collections import deque
 
 n = int(sys.stdin.readline())
 
-arr = [list(map(str, sys.stdin.readline().split())) for _ in range(n)]
+tree = {}
 
-print(arr)
+for n in range(n):
+    root, left, right = sys.stdin.readline().strip().split()
+    tree[root] = [left, right]
 
 
+def preorder(root):
+    if root != '.':
+        print(root, end='')  
+        preorder(tree[root][0])  
+        preorder(tree[root][1])  
+
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])  
+        print(root, end='')  
+        inorder(tree[root][1])  
+
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])  
+        postorder(tree[root][1])  
+        print(root, end='')
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
