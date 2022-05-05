90f9c8041fb2a7ff2167fd9b5a942e4002ecb62c
Keich.Park <svclaw2000@gmail.com>
2022-05-05 18:24:59 +0900

    BOJ 1103 kotlin recursive + dp

--- keich_park/1103.kt
+++ keich_park/1103.kt
@@ -1,64 +1,65 @@
-import java.util.*
-import kotlin.collections.ArrayDeque
+private val dx = listOf(0, 1, 0, -1)
+private val dy = listOf(1, 0, -1, 0)
+private var n = 0
+private var m = 0
+private lateinit var pad: Array<String>
+private lateinit var dp: Array<Array<Int>>
+private var infinite = false
 
-fun main() {
-    val (n, m) = readLine()?.split(' ')?.map { it.toInt() } ?: return
-    val pad = Array(n) { readLine() ?: return }
-    val queue = ArrayDeque<CoinMove>()
-    val dp = Array(n) { Array(m) { -1 } }
-    val dx = listOf(0, 1, 0, -1)
-    val dy = listOf(1, 0, -1, 0)
+private fun main() {
+    val (_n, _m) = readLine()?.split(' ')?.map { it.toInt() } ?: return
+    n = _n
+    m = _m
+    pad = Array(n) { "" }
+    dp = Array(n) { Array(m) { -1 } }
 
-    queue.add(CoinMove(
-        position = 0 to 0,
-        history = setOf()
-    ))
-
-    var maxMove = 0
-    while (queue.isNotEmpty()) {
-        val (position, history) = queue.removeFirst()
-        val (x, y) = position
-        val count = history.size
-
-        // 구멍에 빠짐
-        if (pad[x][y] == 'H') {
-            continue
-        }
-
-        // 현재 위치에서 움직일 수 있으니까
-        maxMove = maxOf(maxMove, count + 1)
-
-        // 방문한 적이 있으면 무한대로 끝
-        if (history.contains(position)) {
-            println(-1)
-            return
-        }
-
-        // 현 위치의 최대 이동 횟수보다 적으면 움직이지 않음
-        if (dp[x][y] >= count + 1) {
-            continue
-        }
-
-        // 최대 이동 횟수 표시
-        dp[x][y] = count + 1
-
-        // 4가지 방향으로 시도
-        (0..3).forEach { i ->
-            val nx = x + dx[i] * (pad[x][y] - '0')
-            val ny = y + dy[i] * (pad[x][y] - '0')
-
-            if (!(nx in 0 until n && ny in 0 until m)) {
-                return@forEach
-            }
-
-            queue.add(CoinMove(nx to ny, history + position))
-        }
+    repeat(n) {
+        pad[it] = readLine() ?: return
     }
 
-    println(maxMove)
+    dfs(0 to 0, setOf())
+
+    if (!infinite) {
+        println(dp.maxOf { it.maxOf { it } })
+    } else {
+        println(-1)
+    }
 }
 
-data class CoinMove(
-    val position: Pair<Int, Int>,
-    val history: Set<Pair<Int, Int>>
-)
+private fun dfs(
+    position: Pair<Int, Int>,
+    history: Set<Pair<Int, Int>>
+) {
+    if (infinite) {
+        return
+    }
+
+    val (x, y) = position
+    val count = history.size
+
+    // 방문한 적이 있으면 무한대로 끝
+    if (history.contains(position)) {
+        infinite = true
+        return
+    }
+
+    // 현 위치의 최대 이동 횟수보다 적으면 움직이지 않음
+    if (dp[x][y] >= count + 1) {
+        return
+    }
+
+    // 최대 이동 횟수 표시
+    dp[x][y] = count + 1
+
+    // 4가지 방향으로 시도
+    (0..3).forEach { i ->
+        val nx = x + dx[i] * (pad[x][y] - '0')
+        val ny = y + dy[i] * (pad[x][y] - '0')
+
+        if (!(nx in 0 until n && ny in 0 until m) || pad[nx][ny] == 'H') {
+            return@forEach
+        }
+
+        dfs(nx to ny, history + position)
+    }
+}
