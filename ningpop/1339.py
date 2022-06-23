18dc25203e78c20a5e67bf7364a13eed233d8202
ningpop <craft1933@naver.com>
2022-06-23 19:42:13 +0900

    [#247] 1339 단어 수학 - 정답

--- ningpop/1339.py
+++ ningpop/1339.py
@@ -1,12 +1,11 @@
 # 2022.06.23
-# 풀이 시간 분 초
-# 채점 결과: 오답 -> 시간 초과
-# 시간복잡도: 
+# 풀이 시간 65분 46초
+# 채점 결과: 오답 -> 시간 초과 -> 정답
+# 시간복잡도: O(N)
 # 문제 링크: https://www.acmicpc.net/problem/1339
 
 import sys
 from collections import defaultdict
-from itertools import permutations
 
 input = sys.stdin.readline
 
@@ -15,26 +14,17 @@ words = []
 for _ in range(n):
     words.append(input().rstrip())
 
-char = []
+frequency = defaultdict(int)
 for word in words:
+    length = len(word) - 1
     for c in word:
-        if c not in char:
-            char.append(c)
+        frequency[c] += pow(10, length)
+        length -= 1
 
-answer = 0
-for case in list(permutations(char, len(char))):
-    number = 9
-    table = defaultdict(int)
-    for c in case:
-        table[c] = number
-        number -= 1
-    
-    result = 0
-    for word in words:
-        num = ''
-        for c in word:
-            num += str(table[c])
-        result += int(num)
-    answer = max(answer, result)
+result = 0
+number = 9
+for key, value in sorted(frequency.items(), key=lambda x : -x[1]):
+    result += (value * number)
+    number -= 1
 
-print(answer)
+print(result)
