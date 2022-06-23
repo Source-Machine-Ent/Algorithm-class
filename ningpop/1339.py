a705c3b0c1c39be8ca55fe97b1715ca2a627b769
ningpop <craft1933@naver.com>
2022-06-23 19:04:25 +0900

    [#247] 1339 단어 수학 - 시간 초과

--- ningpop/1339.py
+++ ningpop/1339.py
@@ -1,45 +1,40 @@
 # 2022.06.23
 # 풀이 시간 분 초
-# 채점 결과: 오답
+# 채점 결과: 오답 -> 시간 초과
 # 시간복잡도: 
 # 문제 링크: https://www.acmicpc.net/problem/1339
 
 import sys
+from collections import defaultdict
+from itertools import permutations
 
 input = sys.stdin.readline
 
 n = int(input())
-word = []
+words = []
 for _ in range(n):
-    word.append(input().rstrip())
+    words.append(input().rstrip())
 
-board = [ [0] * 8 for _ in range(n) ]
-for i in range(n):
-    space = 8 - len(word[i])
-    for j in range(8):
-        if j >= space:
-            board[i][j] = word[i][j - space]
+char = []
+for word in words:
+    for c in word:
+        if c not in char:
+            char.append(c)
 
-word_table = {}
-number = 9
-for j in range(8):
-    for i in range(n):
-        if board[i][j] == 0:
-            continue
-        if board[i][j] in word_table.keys():
-            board[i][j] = word_table[board[i][j]]
-        else:
-            word_table[board[i][j]] = number
-            board[i][j] = number
-            number -= 1
+answer = 0
+for case in list(permutations(char, len(char))):
+    number = 9
+    table = defaultdict(int)
+    for c in case:
+        table[c] = number
+        number -= 1
+    
+    result = 0
+    for word in words:
+        num = ''
+        for c in word:
+            num += str(table[c])
+        result += int(num)
+    answer = max(answer, result)
 
-result = 0
-for i in range(n):
-    num = ''
-    for j in range(8):
-        if board[i][j] == 0:
-            continue
-        num += str(board[i][j])
-    result += int(num)
-print(board)
-print(result)
+print(answer)
